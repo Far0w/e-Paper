f4c90108fce590c6b4dea6e95cd5bff9c1c8e341
Far0w <foulerf@yahoo.com>
2023-02-19 00:16:15 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -10,6 +10,7 @@ import logging
 from waveshare_epd import epd7in5_V2
 from PIL import Image,ImageDraw,ImageFont
 import traceback
+import time
 
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
@@ -22,6 +23,8 @@ logging.basicConfig(level=logging.DEBUG)
 data_collector = dataCollector('https://docs.google.com/spreadsheets/d/e/2PACX-1vRJtzo9q4NS01XynS0s6ic1da7o8sENcO_QCBlt9UbrKw24ltaRj0cdAKcRCSoG3j4-QdSvMJnxBb_i/pub?output=csv')
 calendar_events = data_collector.download_events()
 
+months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 def canva(epd):
     title_font = ImageFont.truetype(os.path.join(fontdir, 'NiceChalk.ttf'), 50)
     text_font = ImageFont.truetype(os.path.join(fontdir, 'Font0.ttc'), 18)
@@ -29,12 +32,13 @@ def canva(epd):
 
     canva1.add_object(Rectangle(0,0,479,72))
     canva1.add_object(Text(title_font, 36, 5, 'SAMEDI 18 FEVRIER', 0, "center"))
+    for event in calendar_events:
+        date_str = str(event[0].day) + " " + months[str(event[0].month)-1] + " | " + str(event[0].hour) + ":" + str(event[0].minute)
+        canva1.add_object(Text(text_font, 36, 5, date_str + event[1], 0, "center"))
 
     canva1.draw_objects()
     return canva1
     
-
-
 try:
     epd = epd7in5_V2.EPD()
     display = Display(epd, picdir, libdir, fontdir)
