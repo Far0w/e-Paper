31f21f58e83f41fbf7f24ad7fa6dce714677759f
Far0w <foulerf@yahoo.com>
2023-02-18 17:59:46 +0100

    minor changes

--- RaspberryPi_JetsonNano/python/scripts/main.py
+++ RaspberryPi_JetsonNano/python/scripts/main.py
@@ -32,7 +32,7 @@ try:
     logging.info("2.Drawing on the Vertical image...")
     Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Limage)
-    draw.text((2, 20), 'Title test', font = title_font, fill = 0)
+    draw.text((2, 5), 'Title test', font = title_font, fill = 0)
     draw.text((2, 50), 'Ecran 7.5 pouces', font = text_font, fill = 0)
     draw.text((2, 80), 'Test 2', font = text_font, fill = 0)
     #draw.line((10, 90, 60, 140), fill = 0)
@@ -44,23 +44,23 @@ try:
     draw.rectangle((10, 150, 60, 200), fill = 0)
     draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
     epd.display(epd.getbuffer(Limage))
-    time.sleep(20)
-
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '7in5_V2.bmp'))
-    epd.display(epd.getbuffer(Himage))
     time.sleep(2)
 
-    logging.info("4.read bmp file on window")
-    Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2))
-    time.sleep(2)
+    #logging.info("3.read bmp file")
+    #Himage = Image.open(os.path.join(picdir, '7in5_V2.bmp'))
+    #epd.display(epd.getbuffer(Himage))
+    #time.sleep(2)
 
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
+    #logging.info("4.read bmp file on window")
+    #Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    #bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+    #Himage2.paste(bmp, (50,10))
+    #epd.display(epd.getbuffer(Himage2))
+    #time.sleep(2)
+
+    #logging.info("Clear...")
+    #epd.init()
+    #epd.Clear()
 
     logging.info("Goto Sleep...")
     epd.sleep()
