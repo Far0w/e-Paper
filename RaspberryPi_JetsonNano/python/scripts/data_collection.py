558cea0737d8f3b39ba0bd122014255db58179fb
Far0w <foulerf@yahoo.com>
2023-02-18 18:58:54 +0000

    added return func

--- RaspberryPi_JetsonNano/python/scripts/data_collection.py
+++ RaspberryPi_JetsonNano/python/scripts/data_collection.py
@@ -8,9 +8,11 @@ class dataCollector:
         
     def download_events(self):
         colnames = ['date','event']
+        events = []
         try:
             df = pd.read_csv(self.events_spreadsheet_URL, names=colnames)
             events = [ [parser.parse(df.iloc[i].date), df.iloc[i].event] for i in range(len(df))]
             print("Events download sucessful. {} events downloaded.".format(len(events)))
         except:
-            print("Events download failed.")
+            print("Events download failed. 0 event downloaded.")
+        return events
