27066e77b5c96d4f2dd61c8a482797babe48cb47
Far0w <foulerf@yahoo.com>
2023-02-25 20:35:53 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -77,7 +77,7 @@ def display_title_date(canva):
 def display_footer(canva):
     quote = random_quote("Nietzsche")
     canva.add_object(Rectangle(0,765,479,35,0))
-    canva.add_object(Text(text_font, 10, 780, quote, 255, "center"))
+    #canva.add_object(Text(text_font, 10, 780, quote, 255, "center"))
     canva.add_object(Text(text_font, 10, 780, "Last update: {}/{} | {}.".format(date.today().strftime("%d"), date.today().strftime("%m"), date.today().strftime("%R")), 255, "center"))
     
 
