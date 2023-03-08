83898ee7d174c75a8f74caaa8fe575709d149e76
Far0w <foulerf@yahoo.com>
2023-03-08 14:28:27 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -83,14 +83,14 @@ def display_footer(canva):
     
 def display_todolist(canva):
     X = 10
-    Y = 320
+    Y = 360
     width = 460
     height = 150
     canva.add_object(Rectangle(X = X, Y = Y, width = width, height = height, fill_color = 225, outline_color = 0, linewidth = 2))
 
 def display_quotes(canva, author = "Marcus Aurelius"):
     X = 10
-    Y = 250
+    Y = 220
     width = 460
     height = 75
     Y_size = 300
